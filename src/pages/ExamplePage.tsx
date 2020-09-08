41df10b16cb0f113cf58e611cc73c7a9bb111fcb
Eric Hao <erichao2018@gmail.com>
2020-09-07 20:07:32 -0700

    Set up some boilerplate structures to represent our songs and songbooks.  (#21)
    * set up structure for getting songs
    
    * get remote file working (i think)
    
    * adjust json file names
    
    * new line
    
    Co-authored-by: Eric Hao <erichao2018@gmail.com>

--- src/pages/ExamplePage.tsx
+++ src/pages/ExamplePage.tsx
@@ -7,9 +7,11 @@ import {
   IonItem,
   IonLabel,
   IonInput,
+  IonButton,
 } from '@ionic/react';
 import React from 'react';
 import ExampleContainer from '../components/ExampleContainer';
+import { Songs } from '../Songs';
 
 // For learning purposes only. Once we have a legit page, delete this one.
 
@@ -29,11 +31,18 @@ const ExamplePage: React.FC = () => {
             <IonInput />
         </IonItem>
 
-
         <ExampleContainer />
+
+        <IonButton onClick={test}></IonButton> { /* test button */ }
       </IonContent>
     </IonPage>
   );
 };
+
+function test()
+{
+  console.log('test');
+  let songs = new Songs();
+}
 
 export default ExamplePage;
