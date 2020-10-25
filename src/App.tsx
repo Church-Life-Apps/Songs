206dd8d7fb645cfed2f04d3d6c58fb03f71dd76d
Brandon Xia <brandonxia01@gmail.com>
2020-10-24 19:39:41 -0500

    make lyric viewer display all lyrics

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IonApp, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/react';
+import { IonApp, IonContent } from '@ionic/react';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
--- src/components/LyricViewer.tsx
+++ src/components/LyricViewer.tsx
@@ -1,3 +1,4 @@
+import { IonItem, IonTextarea } from '@ionic/react';
 import React from 'react';
 import './Components.css';
 
@@ -9,27 +10,41 @@ interface LyricViewProps {
  * Lyric Viewer React Functional Component.
  */
 const LyricViewer: React.FC<LyricViewProps> = (props) => {
-
   var data;
   try {
     data = require(`../../resources/Songs_&_Hymns_Of_Life/metadata/${props.songNumber}.json`);
   } catch {
     return <h1 className="center">No Song Found</h1>;
   }
+  let lyrics = getLyrics(data);
   return (
     <div>
       <h2 className="center">{data['title']}</h2>
       <b>
-          {/* I have no idea how to print these lyrics! 
-          
-          fyi - a couple of the songs use capitals verse numbers for some reason (ie: V1 instead of v1)
-          */}
-        {data['lyrics']['v1']}
+        <IonItem id="lyricTextBox">
+          <IonTextarea readonly value={lyrics} rows={lyrics.split(/\r\n|\r|\n/).length}></IonTextarea>
+        </IonItem>
       </b>
 
-      <footer>Author: {data['author'] == '' ? 'Unknown' : data['author']}</footer>
+      <footer id="lyricViewFooter">Author: {data['author'] == '' ? 'Unknown' : data['author']}</footer>
     </div>
   );
+
+  /**
+   * Parses all verse of the song to a string.
+   */
+  function getLyrics(data: any) {
+    let verses = Object.keys(data['lyrics']);
+    var lyrics = ``;
+    verses.forEach((versenumber) => {
+      lyrics += `\n${versenumber}: `;
+      data['lyrics'][versenumber].forEach((line: String) => {
+        lyrics += `\t${line}\n`;
+      });
+    });
+    lyrics = lyrics.trimStart()
+    return lyrics;
+  }
 };
 
 export default LyricViewer;
