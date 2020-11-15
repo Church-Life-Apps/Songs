bbdd82681dd428b3d5df146ba12357c37325181f
Brandon Xia <brandonxia01@gmail.com>
2020-11-15 16:20:53 -0600

    Add some utils and test cases

--- src/utils/SecurityUtils.tsx
+++ src/utils/SecurityUtils.tsx
@@ -8,12 +8,12 @@ let CryptoJS = require("crypto-js");
  * Uses AES to encrypt some text with the given private key.
  */
 export function encrypt(normalText: String, privateKey: String) {
-    return CryptoJS.AES.encrypt(normalText, privateKey);
+  return CryptoJS.AES.encrypt(normalText, privateKey);
 }
 
 /**
  * Uses AES to decrypt some text with the given private key.
  */
 export function decrypt(encryptedtext: String, privateKey: String) {
-   return CryptoJS.AES.decrypt(encryptedtext, privateKey).toString(CryptoJS.enc.Utf8);
+  return CryptoJS.AES.decrypt(encryptedtext, privateKey).toString(CryptoJS.enc.Utf8);
 }
---
+++ src/utils/TimeUtils.tsx
@@ -0,0 +1,63 @@
+/**
+ * Utility functions related to time.
+ */
+
+export const MILLIS_PER_SECOND = 1000;
+export const MILLIS_PER_MINUTE = 60 * MILLIS_PER_SECOND;
+export const MILLIS_PER_HOUR = 60 * MILLIS_PER_MINUTE;
+export const MILLIS_PER_DAY = 24 * MILLIS_PER_HOUR;
+export const MILLIS_PER_WEEK = 7 * MILLIS_PER_DAY;
+export const MILLIS_PER_MONTH = 30 * MILLIS_PER_DAY;
+export const MILLIS_PER_YEAR = 365 * MILLIS_PER_DAY;
+
+/**
+ * Turns a timestamp from milliseconds to a formatted Date.
+ */
+export function formatDate(timeInMillis: number): string {
+  var date = new Date(timeInMillis);
+  return date.toUTCString();
+}
+
+/** 
+ * Returns how long ago a timestamp was from now, or how far into the future it is.
+ */
+export function describeTime(timeInMillis: number): string {
+  let howLongAgo = Date.now() - timeInMillis;
+  let past = true;
+  if (howLongAgo < 0) {
+    past = false;
+    howLongAgo = howLongAgo * -1;
+  }
+  let number = 0;
+  let unit = '';
+  if (howLongAgo > MILLIS_PER_DAY) {
+    number = Math.round(howLongAgo / MILLIS_PER_DAY);
+    unit = 'days';
+    if (number === 1) {
+      unit = 'day';
+    }
+  } else if (howLongAgo > MILLIS_PER_HOUR) {
+    number = Math.round(howLongAgo / MILLIS_PER_HOUR);
+    unit = 'hours';
+    if (number === 1) {
+      unit = 'hour';
+    }
+  } else if (howLongAgo > MILLIS_PER_MINUTE) {
+    number = Math.round(howLongAgo / MILLIS_PER_MINUTE);
+    unit = 'minutes';
+    if (number === 1) {
+      unit = 'minute';
+    }
+  } else {
+    number = Math.round(howLongAgo / MILLIS_PER_SECOND);
+    unit = 'seconds';
+    if (number === 1) {
+      unit = 'second';
+    }
+  }
+  if (past) {
+    return `${number} ${unit} ago.`;
+  } else {
+    return `In ${number} ${unit}.`;
+  }
+}
