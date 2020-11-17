2df34091af86d6eadb693daccd62c75b55460e1a
Brandon Xia <brandonxia01@gmail.com>
2020-11-16 22:57:41 -0600

    update music view ui

--- src/components/MusicView.tsx
+++ src/components/MusicView.tsx
@@ -4,8 +4,7 @@ import { makeThreeDigits } from "../utils/SongUtils";
 import { IonToggle } from "@ionic/react";
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
-const baseUrl =
-  "https://raw.githubusercontent.com/Church-Life-Apps/Resources/master/";
+const baseUrl = "https://raw.githubusercontent.com/Church-Life-Apps/Resources/master/";
 const hymnalPart = "SongsAndHymnsOfLife/SHL_"; // This part can change when red book is added
 const imageSuffix = ".png";
 const alt = "No Song Found";
@@ -21,38 +20,42 @@ interface MusicViewProps {
  * Song Viewer React Functional Component.
  */
 const MusicView: React.FC<MusicViewProps> = (props) => {
+  const widthPixels = 700;
   const [secondTune, setSecondTune] = useState<boolean>(false);
+  const [width, setWidth] = useState<number>(widthPixels);
+  const [zoomed, setZoomed] = useState<boolean>(false);
 
   let songHasTwoTunes = songsWithTwoTunes.includes(props.songNumber);
 
   let secondTuneSuffix = songHasTwoTunes && secondTune ? "-B" : "";
 
-  let url =
-    baseUrl +
-    hymnalPart +
-    makeThreeDigits(props.songNumber) +
-    secondTuneSuffix +
-    imageSuffix;
+  let url = baseUrl + hymnalPart + makeThreeDigits(props.songNumber) + secondTuneSuffix + imageSuffix;
 
   // TODO: Add Pinch and Zoom to image.
   return (
-    <div>
+    <div style={{ maxWidth: width }} id="musicViewDiv">
       {/* Second Tune Toggler  */}
       {songHasTwoTunes ? (
         <div id="songTogglerDiv">
-          <IonToggle
-            checked={secondTune}
-            onIonChange={(e) => setSecondTune(!secondTune)}
-          ></IonToggle>
+          <IonToggle checked={secondTune} onIonChange={(e) => setSecondTune(!secondTune)}></IonToggle>
         </div>
       ) : null}
 
       {/* image */}
-      <TransformWrapper>
-        <TransformComponent>
-          <img src={url} alt={alt} />
-        </TransformComponent>
-      </TransformWrapper>
+      {/* <TransformWrapper>
+        <TransformComponent> */}
+      <img
+        onDoubleClick={() => {
+          if (zoomed) {
+            setWidth(widthPixels);
+          } else setWidth(widthPixels * 2);
+          setZoomed(!zoomed);
+        }}
+        src={url}
+        alt={alt}
+      />
+      {/* </TransformComponent>
+      </TransformWrapper> */}
     </div>
   );
 };
