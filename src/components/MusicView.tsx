f5ad5e411fc2079fa4f901393701c74f2780e73e
brandonxia01 <brandonxia01@gmail.com>
2020-11-17 13:00:15 -0600

    Update music view ui (#47)
    * update music view ui
    
    * adjust UI to show the 2 tune toggler again
    
    * remove the import

--- src/components/MusicView.tsx
+++ src/components/MusicView.tsx
@@ -2,10 +2,8 @@ import React, { useState } from "react";
 import "./Components.css";
 import { makeThreeDigits } from "../utils/SongUtils";
 import { IonToggle } from "@ionic/react";
-import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
-const baseUrl =
-  "https://raw.githubusercontent.com/Church-Life-Apps/Resources/master/";
+const baseUrl = "https://raw.githubusercontent.com/Church-Life-Apps/Resources/master/";
 const hymnalPart = "SongsAndHymnsOfLife/SHL_"; // This part can change when red book is added
 const imageSuffix = ".png";
 const alt = "No Song Found";
@@ -21,38 +19,44 @@ interface MusicViewProps {
  * Song Viewer React Functional Component.
  */
 const MusicView: React.FC<MusicViewProps> = (props) => {
+  const widthPixels = 700;
   const [secondTune, setSecondTune] = useState<boolean>(false);
+  const [width, setWidth] = useState<number>(widthPixels);
+  const [zoomed, setZoomed] = useState<boolean>(false);
 
   let songHasTwoTunes = songsWithTwoTunes.includes(props.songNumber);
 
   let secondTuneSuffix = songHasTwoTunes && secondTune ? "-B" : "";
 
-  let url =
-    baseUrl +
-    hymnalPart +
-    makeThreeDigits(props.songNumber) +
-    secondTuneSuffix +
-    imageSuffix;
+  let url = baseUrl + hymnalPart + makeThreeDigits(props.songNumber) + secondTuneSuffix + imageSuffix;
 
   // TODO: Add Pinch and Zoom to image.
   return (
     <div>
       {/* Second Tune Toggler  */}
-      {songHasTwoTunes ? (
+      {songHasTwoTunes && (
         <div id="songTogglerDiv">
-          <IonToggle
-            checked={secondTune}
-            onIonChange={(e) => setSecondTune(!secondTune)}
-          ></IonToggle>
+          <IonToggle checked={secondTune} onIonChange={(e) => setSecondTune(!secondTune)}></IonToggle>
         </div>
-      ) : null}
+      )}
 
       {/* image */}
-      <TransformWrapper>
-        <TransformComponent>
-          <img src={url} alt={alt} />
-        </TransformComponent>
-      </TransformWrapper>
+      {/* <TransformWrapper>
+        <TransformComponent> */}
+      <img
+        style={{ maxWidth: width }}
+        id="musicViewDiv"
+        onDoubleClick={() => {
+          if (zoomed) {
+            setWidth(widthPixels);
+          } else setWidth(widthPixels * 2);
+          setZoomed(!zoomed);
+        }}
+        src={url}
+        alt={alt}
+      />
+      {/* </TransformComponent>
+      </TransformWrapper> */}
     </div>
   );
 };
