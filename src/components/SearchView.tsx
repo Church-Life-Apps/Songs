4397f28b444f525aab9b7e9294e43680622dbbb6
Eric Hao <hao.awesome@gmail.com>
2020-11-19 13:19:55 -0800

    preprocess search param once.

--- src/components/SearchView.tsx
+++ src/components/SearchView.tsx
@@ -1,13 +1,9 @@
-import {
-  IonList,
-  IonCard,
-  IonCardTitle,
-  IonCardSubtitle,
-} from "@ionic/react";
+import { IonList, IonCard, IonCardTitle, IonCardSubtitle } from "@ionic/react";
 import React from "react";
 import { useParams, useHistory } from "react-router-dom";
 import { removePunctuation } from "../utils/SongUtils";
 import "./Components.css";
+import songs from "../resources/Songs_&_Hymns_Of_Life/BlackBookSongList.json";
 
 interface SearchViewProps {
   searchString: any;
@@ -25,16 +21,42 @@ interface Song {
 const SearchView: React.FC<SearchViewProps> = (props) => {
   const { bookId } = useParams<{ bookId: string }>();
   let history = useHistory();
-  let songs = require("../resources/Songs_&_Hymns_Of_Life/BlackBookSongList.json");
+
+  let searchParam = GetSearchParam();
+  let searchIsEmpty = searchParam === undefined;
+  let searchIsNumber = typeof searchParam === "number";
+
   let songCards = GetSongsList(songs.songs);
 
   return <IonList>{songCards}</IonList>;
 
-  // generate all the songs as IonCards. This should only be called once and saved.
+  function GetSearchParam() {
+    if (
+      props.searchString === undefined ||
+      typeof props.searchString !== "string" ||
+      props.searchString.trim() === ""
+    ) {
+      return undefined;
+    }
+
+    let searchString = removePunctuation(
+      props.searchString.trim().toLowerCase()
+    );
+    let searchNumber = Number(searchString);
+
+    if (!isNaN(searchNumber)) {
+      return searchNumber;
+    }
+
+    return searchString.split(" ");
+  }
+
+  // generate all the songs as IonCards.
   function GetSongsList(songs: Song[]) {
+    console.log("GetSongsList");
     let GenerateIonItem = (song: Song) => {
       return (
-        SongMatchesSearch(song, props.searchString) && (
+        SongMatchesSearch(song, searchParam, searchIsEmpty, searchIsNumber) && (
           <IonCard
             key={song.songNumber}
             onClick={() => {
@@ -53,27 +75,24 @@ const SearchView: React.FC<SearchViewProps> = (props) => {
     return songs.map(GenerateIonItem);
   }
 
-  function SongMatchesSearch(song: Song, searchString: any): boolean {
-    if (
-      searchString === undefined ||
-      typeof searchString !== "string" ||
-      searchString.trim() === ""
-    ) {
+  function SongMatchesSearch(
+    song: Song,
+    searchParam: any,
+    searchIsEmpty: boolean,
+    searchIsNumber: boolean
+  ): boolean {
+    if (searchIsEmpty) {
       return true;
     }
 
-    searchString = removePunctuation(searchString.trim().toLowerCase());
-    let searchNumber = Number(searchString);
-
-    if (!isNaN(searchNumber)) {
-      return searchNumber === song.songNumber;
+    if (searchIsNumber) {
+      return searchParam === song.songNumber;
     }
 
     let title = removePunctuation(song.title.toLowerCase());
     let author = removePunctuation(song.author.toLowerCase());
 
-    let searchTerms: string[] = searchString.split(" ");
-    for (let s of searchTerms) {
+    for (let s of searchParam) {
       if (!title.includes(s) && !author.includes(s)) {
         return false;
       }
