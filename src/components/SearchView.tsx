80120b8a4f8b6522b50a922d18dbd958ee4a1874
Eric Hao <hao.awesome@gmail.com>
2020-11-19 23:53:52 -0800

    infinite scroll working!

--- src/components/SearchView.tsx
+++ src/components/SearchView.tsx
@@ -5,8 +5,10 @@ import {
   IonCardSubtitle,
   IonLabel,
   IonItem,
+  IonInfiniteScroll,
+  IonInfiniteScrollContent,
 } from "@ionic/react";
-import React from "react";
+import React, { useState } from "react";
 import { useParams, useHistory } from "react-router-dom";
 import { removePunctuation } from "../utils/SongUtils";
 import "./Components.css";
@@ -27,30 +29,66 @@ interface Song {
  */
 const SearchView: React.FC<SearchViewProps> = (props) => {
   const { bookId } = useParams<{ bookId: string }>();
+  const [songCards, setSongCards] = useState<JSX.Element[]>([]);
+  const [songCardsIterator] = useState(songs.songs.entries());
+
   let history = useHistory();
-
   let searchParam = GetSearchParam();
-  let searchIsEmpty = searchParam === undefined;
-  let searchIsNumber = typeof searchParam === "number";
 
-  let songCards: JSX.Element[];
-  if (searchIsEmpty) {
-    songCards = [];
-  } else if (searchIsNumber) {
-    songCards = [GenerateSongCard(songs.songs[(searchParam as number) - 1])];
+  if (typeof searchParam === "number") {
+    setSongCards([GenerateSongCard(songs.songs[(searchParam as number) - 1])]);
   } else {
-    songCards = songs.songs
-      .filter((s) => SongMatchesSearch(s, searchParam))
-      .map((s) => GenerateSongCard(s) as JSX.Element);
+    if (songCards.length === 0) {
+      LoadSongs(songCardsIterator, 20, searchParam as string[]);
+    }
   }
 
   return songCards.length > 0 ? (
-    <IonList>{songCards}</IonList>
+    <div>
+      <IonList>{songCards}</IonList>
+      <IonInfiniteScroll onIonInfinite={LoadMoreSongs}>
+        <IonInfiniteScrollContent
+          loadingSpinner="bubbles"
+          loadingText="Loading more songs..."
+        ></IonInfiniteScrollContent>
+      </IonInfiniteScroll>
+    </div>
   ) : (
     <IonItem>
       <IonLabel>No results found</IonLabel>
     </IonItem>
   );
+
+  function LoadMoreSongs(event: CustomEvent<void>) {
+    let target = event.target as HTMLIonInfiniteScrollElement;
+    if (!LoadSongs(songCardsIterator, 10, searchParam as string[])) {
+      target.disabled = true;
+    }
+    setSongCards(Array.from(songCards));
+    target.complete();
+  }
+
+  function LoadSongs(
+    songIterator: IterableIterator<[number, Song]>,
+    count: number,
+    searchParam: string[]
+  ): boolean {
+    while (count > 0) {
+      let nextSong = songIterator.next();
+      if (nextSong.done) {
+        return false;
+      }
+
+      let song = nextSong.value[1];
+
+      if (SongMatchesSearch(song, searchParam)) {
+        songCards.push(GenerateSongCard(song));
+        count--;
+      }
+    }
+
+    return true;
+  }
 
   function GetSearchParam() {
     if (
@@ -93,7 +131,11 @@ const SearchView: React.FC<SearchViewProps> = (props) => {
     );
   }
 
-  function SongMatchesSearch(song: Song, searchParam: any): boolean {
+  function SongMatchesSearch(song: Song, searchParam: string[]): boolean {
+    if (searchParam === undefined || searchParam.length === 0) {
+      return true;
+    }
+
     let title = removePunctuation(song.title.toLowerCase());
     let author = removePunctuation(song.author.toLowerCase());
 
