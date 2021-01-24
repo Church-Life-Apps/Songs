62f8e80dc3fa7d46c03ebee3ed84deb30da25972
brandonxia01 <brandonxia01@users.noreply.github.com>
2021-01-24 04:28:50 +0000

    Prettified Code!

--- src/utils/StringUtils.tsx
+++ src/utils/StringUtils.tsx
@@ -24,5 +24,5 @@ export function getSimilarity(word: string, search: string): number {
  * Returns true if a string is a number, false otherwise.
  */
 export function isNumeric(word: string): boolean {
-  return !isNaN(+word)
+  return !isNaN(+word);
 }
