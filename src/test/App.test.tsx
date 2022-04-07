3a5a1064516f0973e4bb0664adeab98bf714b13a
brandonxia01 <brandonxia01@gmail.com>
2022-04-06 23:58:27 -0500

    Auto select song book if theres only 1 songbook (#163)
    * if only 1 book then auto select it
    
    * Prettified Code!
    
    * address comments
    
    Co-authored-by: brandonxia01 <brandonxia01@users.noreply.github.com>

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,5 +1,4 @@
 import puppeteer, { Browser, Page } from "puppeteer";
-import { defaultNavigationTitle } from "../components/NavigationBar";
 import { exception } from "console";
 
 const baseUrl = "http://localhost:8080";
@@ -18,6 +17,7 @@ const selectors = {
   nextButton: "#nextButton",
   prevButton: "#prevButton",
 };
+const hasMultipleBooks = false;
 
 describe("App", () => {
   let page: Page;
@@ -40,7 +40,7 @@ describe("App", () => {
     await page.waitForSelector(selectors.appName);
 
     const html = await page.$eval(selectors.appName, (e) => e.innerHTML);
-    expect(html).toBe(defaultNavigationTitle);
+    expect(html).toBeTruthy();
   });
 
   it("this test helps prevent later tests from failing", () => {
@@ -95,8 +95,10 @@ describe("App", () => {
   });
 
   it("selecting song displays song page music view", async () => {
-    await page.waitForSelector(selectors.shlSongbook);
-    await page.click(selectors.shlSongbook);
+    if (hasMultipleBooks) {
+      await page.waitForSelector(selectors.shlSongbook);
+      await page.click(selectors.shlSongbook);
+    }
 
     await page.waitForSelector(selectors.searchViewIonCardTitle);
 
@@ -121,8 +123,10 @@ describe("App", () => {
   }, 20000);
 
   it("song page lyric view works correctly", async () => {
-    await page.waitForSelector(selectors.shlSongbook);
-    await page.click(selectors.shlSongbook);
+    if (hasMultipleBooks) {
+      await page.waitForSelector(selectors.shlSongbook);
+      await page.click(selectors.shlSongbook);
+    }
 
     await page.waitForSelector(selectors.searchViewIonCardTitle);
 
@@ -171,8 +175,10 @@ describe("App", () => {
   });
 
   it("displays song list and loads all songs on scroll", async () => {
-    await page.waitForSelector(selectors.shlSongbook);
-    await page.click(selectors.shlSongbook);
+    if (hasMultipleBooks) {
+      await page.waitForSelector(selectors.shlSongbook);
+      await page.click(selectors.shlSongbook);
+    }
 
     await page.waitForSelector(selectors.searchViewIonCard);
 
@@ -199,8 +205,10 @@ describe("App", () => {
   }, 20000);
 
   it("displays arrow buttons and transitions correctly on lyrics mode", async () => {
-    await page.waitForSelector(selectors.shlSongbook);
-    await page.click(selectors.shlSongbook);
+    if (hasMultipleBooks) {
+      await page.waitForSelector(selectors.shlSongbook);
+      await page.click(selectors.shlSongbook);
+    }
 
     await page.waitForSelector(selectors.searchViewIonCardTitle);
 
@@ -228,8 +236,10 @@ describe("App", () => {
   });
 
   it("displays arrow buttons and transitions correctly on music mode", async () => {
-    await page.waitForSelector(selectors.shlSongbook);
-    await page.click(selectors.shlSongbook);
+    if (hasMultipleBooks) {
+      await page.waitForSelector(selectors.shlSongbook);
+      await page.click(selectors.shlSongbook);
+    }
 
     await page.waitForSelector(selectors.searchViewIonCardTitle);
 
@@ -277,8 +287,10 @@ describe("App", () => {
  * If strict = false, then only the top N song cards must match the given songResults, where N = songResults.length.
  */
 async function verifySearchResults(page: Page, searchTerm: string, songResults: string[], strict = true) {
-  await page.waitForSelector(selectors.shlSongbook);
-  await page.click(selectors.shlSongbook);
+  if (hasMultipleBooks) {
+    await page.waitForSelector(selectors.shlSongbook);
+    await page.click(selectors.shlSongbook);
+  }
 
   if (songResults !== null && songResults.length >= 20) {
     throw exception("verifySearchResults only works if songResults < 20 items.");
